81ed46746bcc9d82cc70e8832470acb9f8540d34
Aditya Lamba <lambaaditya24@gmail.com>
2023-02-21 18:41:32 -0700

    first commit - setting routes

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const NewPlace =()=>{
+    return <h1>This is new place</h1>
+}
+
+export default NewPlace;
---
+++ src/user/pages/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Users = () => {
+
+    return (
+    <h1>This is Users Page</h1>
+    );
+}
+
+
+export default Users;
